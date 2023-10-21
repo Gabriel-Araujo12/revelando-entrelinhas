a68484f40fa59a7c550614034699136365b41281
Gabriel Araújo <85949953+Gabriel-Araujo12@users.noreply.github.com>
2023-10-20 23:59:18 -0300

    Nova tentativa de KNN
    Foi realizada uma tentativa bem-sucedida para aplicar o KNN na base de dados a fim de testar o seu funcionamento.

--- Projeto/main.py
+++ Projeto/main.py
@@ -2,6 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score 
@@ -11,16 +12,20 @@ path = (r"C:\Users\gabri\Downloads\TCC\Projeto\sinresp.csv")
 df = pd.read_csv(path, on_bad_lines='skip', sep=';', low_memory=False)
 
 #Pré-processamento dos dados da base
-
 limite = len(df) * 0.5
 df = df.dropna(thresh = limite, axis = 1)
 df = df.dropna(subset = ['EVOLUCAO'])
 
-colunas_apagadas = ['SG_UF_NOT', 'ID_REGIONA', 'ID_MUNICIP', 'ID_UNIDADE', 'ID_PAIS', 'SG_UF', 'ID_RG_RESI', 'ID_MN_RESI', 'DT_NOTIFIC', 'DT_NASC']
+colunas_apagadas = ['SG_UF_NOT', 'ID_REGIONA', 'ID_MUNICIP', 'ID_UNIDADE', 'ID_PAIS', 'SG_UF', 'ID_RG_RESI', 'ID_MN_RESI', 'DT_NOTIFIC', 'DT_NASC', 'DT_NOTIFIC', 'DT_SIN_PRI', 'DT_NASC', 'DT_INTERNA', 'DT_COLETA', 'DT_PCR', 'DT_EVOLUCA', 'DT_ENCERRA', 'DT_DIGITA', 'CS_SEXO', 'SG_UF_INTE', 'ID_RG_INTE', 'ID_MN_INTE']
 X = df.drop(columns = colunas_apagadas)
 
-#Aplicação do KNN
+#Convertendo todas as colunas restantes do DataFrame para valores numéricos
+X = X.apply(pd.to_numeric, errors='coerce')
 
+#Preenchendo os valores ausentes das colunas com sua média
+X = X.fillna(X.mean())
+
+#Aplicação do KNN
 Y = df.EVOLUCAO
 
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 10)
@@ -30,3 +35,9 @@ clf.fit(x_train, y_train)
 
 pred_scores = clf.predict_proba(x_test)
 print(pred_scores)
+
+y_pred = clf.predict(x_test)
+te_acc= (sklearn.metrics.accuracy_score(y_test, y_pred)) 
+print('Acurácia obtida: ', te_acc)
+
+print(X.shape)
