62f19267f076e2fb9d4067e5e78b3148c5e0966b
Gabriel Araújo <85949953+Gabriel-Araujo12@users.noreply.github.com>
2023-11-14 12:28:04 -0300

    KNN na base original
    O algoritmo de previsão KNN foi aplicado na base original.

---
+++ Projeto/main_bo.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.impute import KNNImputer
+from sklearn import metrics
+
+path = (r"C:\Users\gabri\Downloads\TCC\Projeto\sinresp.csv")
+df = pd.read_csv(path, on_bad_lines='skip', sep=';', low_memory=False)
+
+#Convertendo colunas não numéricas para numéricas
+le = LabelEncoder()
+colunas_nn = df.select_dtypes(exclude=['number']).columns
+
+while len(colunas_nn) > 0:
+    column = colunas_nn[0]
+    df[column] = le.fit_transform(df[column])
+    colunas_nn = df.select_dtypes(exclude=['number']).columns
+
+#Preenchendo valores nulos como 0
+df = df.fillna(0)
+
+#Pré-processamento dos dados da base
+X = df
+
+# Aplicação do KNN
+Y = df.EVOLUCAO
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=10)
+
+clf = KNeighborsClassifier(n_neighbors=11)
+clf.fit(x_train, y_train)
+
+pred_scores = clf.predict_proba(x_test)
+print(pred_scores)
+
+y_pred = clf.predict(x_test)
+te_acc = metrics.accuracy_score(y_test, y_pred)
+print('Acurácia obtida: ', te_acc)
+
+print(X.shape)
